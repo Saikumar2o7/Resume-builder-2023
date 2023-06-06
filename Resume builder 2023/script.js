24979cdb4fae279afef05b96746c8d92b7b5bb61
Saikumar2o7 <132538950+Saikumar2o7@users.noreply.github.com>
2023-06-06 18:57:15 +0530

    Add files via upload

---
+++ Resume builder 2023/script.js
@@ -0,0 +1,7 @@
+window.onresize = screen;
+window.onload = screen;
+
+function screen(){
+    var myWidth = window.innerWidth;
+        document.querySelector('#width').innerHTML = " width : " + myWidth + "";
+    }
